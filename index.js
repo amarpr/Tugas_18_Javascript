719d15dc5a7cf420ff57204f08bf02da5e28a0dc
Muhamar <amararrasyid@gmail.com>
2020-05-12 17:58:56 +0700

    Tugas 18 Javascript
    Mengumpulkan Tugas Niomic

---
+++ index.js
@@ -0,0 +1,7 @@
+function kodeAt() {
+    var kode = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam distinctio vel suscipit saepe omnis eveniet odit vero nemo, illum porro sequi sapiente officia, commodi non, repellat aspernatur ea adipisci assumenda.";
+
+    console.log(kode.charCodeAt(0) + "-" + kode.charCodeAt(1) + "-" + kode.charCodeAt(2) + "-" + kode.charCodeAt(3) + "-" + kode.charCodeAt(4) + "-" + kode.charCodeAt(6));
+}
+
+kodeAt()
